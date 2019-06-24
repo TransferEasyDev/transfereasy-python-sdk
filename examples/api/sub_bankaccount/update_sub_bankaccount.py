f3a47e4e31c61f6f9578feb1ade50c96cdcb9b64
kongfm <kongp3@outlook>
2019-06-24 15:09:50 +0800

    根据文档更新Demo

---
+++ examples/api/sub_bankaccount/update_sub_bankaccount.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    import json
+    from kernel.api.sub_bankaccount import SubBankAccount
+
+    sbacc = SubBankAccount()
+    params = {
+        'country': "HKG",
+        'shop': {
+                'type': 'AMAZON',
+                'name': 'name',
+                'industry': u'旅行，运输，住宿及物流',
+                'url': 'https://www.amazon.com/xxxx',
+                'piece_transfer_amount': 1000,
+                'week_transfer_amount': 1000,
+                'amazon_site': 'USA',
+                'business_description': 'business_description',
+                'seller_id': 'seller_id',
+                'access_key': 'access_key',
+                'secret_key': 'secret_key',
+        }
+    }
+
+    sbacc.create_sub_bankaccount(params)
